5871ceea49e7455ef366f1bdf8e98aff9c248747
MrKB03-creator <badillokurt9@gmail.com>
2024-07-14 15:39:33 -0700

    Exercise 1 to 4 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num, num2, num3, num4) {
+    const arr = array;
+    let newArr = [];
+
+    for(let j = 0 ; j < arr.length; j++) {
+        if(arr[j] !== num && arr[j] !== num2 && arr[j] !== num3 && arr[j] != num4) {
+            newArr.push(arr[j]);
+        }
+    }
+    return newArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
