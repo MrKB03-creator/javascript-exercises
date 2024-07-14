01fc37ad8e5e8012c6cbef81628ad445110d9c2c
MrKB03-creator <badillokurt9@gmail.com>
2024-07-14 15:40:07 -0700

    Exercise 1 to 4 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let st = string;
+    let num1 = num;
 
+    if(num1 > 0){
+        for(let i = 1; i < num1; i++){
+            st += string;
+        }
+        return st;
+    }
+    else if(num1 == 0){
+        return '';
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray =  word.split('');
+    let reverseArray = wordArray.reverse().join('');
+    return reverseArray;
 };
 
 // Do not edit below this line
