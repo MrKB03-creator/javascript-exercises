6081105c9d87651761ff4a3e842a544c1ffcb38f
MrKB03-creator <badillokurt9@gmail.com>
2024-07-15 21:26:52 -0700

    Calculator Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumNumber = 0
+	for(let i =  0; i < arr.length; i ++) {
+    sumNumber += arr[i];
+  }
+  return sumNumber;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+  for(let i = 0; i < arr.length; i++){
+    mult *= arr[i];
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = [];
+	if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
 };
 
 // Do not edit below this line
