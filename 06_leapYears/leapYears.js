248ceda313d38c6e1b03b5899f798f488d8c0838
MrKB03-creator <badillokurt9@gmail.com>
2024-07-15 18:56:51 -0700

    Exercises 6 and 7 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    console.log(year%100)
+    if(year%100 > 0 && year%4 === 0) {
+        console.log(true);
+        return true;
+    } else {
+        if(year%400 === 0) {
+            console.log(true);
+            return true;
+        }
+        console.log(false);
+        return false;
+    }
 };
 
+
+
+leapYears(34992)
 // Do not edit below this line
 module.exports = leapYears;
